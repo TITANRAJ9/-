3051a3b834cc0b63c3e2c91ede31d5406d75dbf8
TITANRAJ9 <rajwardhanmohite9193@gmail.com>
2026-05-03 15:10:47 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Void MC is the ultimate all-in-one Discord bot designed by TITANRAJ. ",
+        name: "Void MC | 0 players ",
         // Activity type number (0 = Playing).
         type: 3, 
       },
